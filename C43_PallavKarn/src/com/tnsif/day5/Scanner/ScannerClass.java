da536b77f01b3372e0b4d9f3beb5c4b155c11314
palla <palla@DESKTOP-2JN66LF>
2024-08-14 12:19:13 +0530

    commit

--- C43_PallavKarn/src/com/tnsif/day5/Scanner/ScannerClass.java
+++ C43_PallavKarn/src/com/tnsif/day5/Scanner/ScannerClass.java
@@ -1,10 +1,23 @@
 package com.tnsif.day5.Scanner;
 
+import java.util.Scanner;
+
 public class ScannerClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        System.out.println("Enter your name:");
+        String a = sc.nextLine();
 
-	}
+        System.out.println("Enter your Department:");
+        String b = sc.nextLine();
 
+        System.out.println("Enter your year:");
+        String c = sc.nextLine();
+
+        System.out.println("Your name is " +a);
+        System.out.println("Your department is " + b);
+        System.out.println("Your year is " + c);
+        System.out.println("Your name is "+ a + "department is "+ b + "year is "+ c);
+    }
 }
