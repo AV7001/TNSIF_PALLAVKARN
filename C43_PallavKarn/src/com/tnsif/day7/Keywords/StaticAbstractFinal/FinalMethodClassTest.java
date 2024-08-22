0d4a2ca41826bb245af9fdf9d2010bda682807e5
palla <palla@DESKTOP-2JN66LF>
2024-08-22 21:34:33 +0530

    comit

--- C43_PallavKarn/src/com/tnsif/day7/Keywords/StaticAbstractFinal/FinalMethodClassTest.java
+++ C43_PallavKarn/src/com/tnsif/day7/Keywords/StaticAbstractFinal/FinalMethodClassTest.java
@@ -1,5 +1,11 @@
 package com.tnsif.day7.Keywords.StaticAbstractFinal;
 
-public class FinalMethodClassTest {
-
+public class FinalMethodClassTest extends FinalMethodClass  {
+	  // Compile time error because we cannot override the final method 
+		/*
+		 * @Override void show() {
+		 * 
+		 * System.out.println("This is the final method of FinalMethodEx class"); }
+		 */
+	   
 }
