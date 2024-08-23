16e813de6448eb1f65066df4cf26c76901d51f39
palla <palla@DESKTOP-2JN66LF>
2024-08-23 12:16:13 +0530

    comit

--- C43_PallavKarn/src/com/tnsif/day9/Interfaces/WrapperClasses/WrapperClasses.java
+++ C43_PallavKarn/src/com/tnsif/day9/Interfaces/WrapperClasses/WrapperClasses.java
@@ -1,5 +1,14 @@
 package com.tnsif.day9.Interfaces.WrapperClasses;
 
 public class WrapperClasses {
-
+    public static void main(String[] args) {
+		
+    	//Unboxing - COnverting explicity
+    	Integer i= new Integer(10);
+    	System.out.println(i);
+    	int b=i.intValue();
+    	System.out.println(b);
+    	
+    	
+	}
 }
